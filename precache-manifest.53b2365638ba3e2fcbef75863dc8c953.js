d786889183f7c3cf3e93df6bd2582915d4d4bcac
System Administrator <root@logans-Mac-mini.local>
2020-01-05 15:43:24 -0600

    Updates

---
+++ precache-manifest.53b2365638ba3e2fcbef75863dc8c953.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "549f671aeb77c724668d47864f0fc6e4",
+    "url": "/Incredible-WebApp-/index.html"
+  },
+  {
+    "revision": "51c62e870b96d85521b9",
+    "url": "/Incredible-WebApp-/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "51c62e870b96d85521b9",
+    "url": "/Incredible-WebApp-/static/js/2.530906e7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Incredible-WebApp-/static/js/2.530906e7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ab481b5225332103221c",
+    "url": "/Incredible-WebApp-/static/js/main.6573ddbf.chunk.js"
+  },
+  {
+    "revision": "0fa62fdc1265b4d6ad4c",
+    "url": "/Incredible-WebApp-/static/js/runtime-main.daf02aa5.js"
+  }
+]);
